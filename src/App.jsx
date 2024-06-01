86b46c080eda6d42d16e7f79317403a50ddb77c1
Shehan Samarasinghe <shehan.akhs@gmail.com>
2024-06-02 09:28:00 +1000

    Adding Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout/>}>
+      <Route index element={<HomePage/>}/>
+    </Route>
+
+)
+);
+
 function App() {
-  
   return (
-    <div>
-      App
-    </div>
-    
+    <RouterProvider router={router} />
   )
 }
 
